c3ec785a39e370773fe89fadbe300556e102fd2e
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-08-03 18:42:19 -0400

    update app

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# kraken-portfolio
-Kraken Crypto Portfolio App
+# Kraken API Portfolio Tracker
+
+This Streamlit app is a demo of using the Kraken API within a Streamlit app to keep track of your cryptocurrency and fiat currency positions. It fetches your account balances from the Kraken exchange and presents them in an interactive scatter plot along with the current price of each cryptocurrency in USD.
+
+## Overview
+
+The Kraken API Portfolio Tracker is a simple app that allows you to visualize your cryptocurrency holdings and their respective values. The app communicates with the Kraken API to fetch your account balances, which are then displayed in an interactive scatter plot.
+
+## How to Run the App
+
+1. Make sure you have set up your Kraken API key and secret in your environment variables (`API_KEY_KRAKEN` and `API_SEC_KRAKEN`).
+2. Clone https://github.dev/mattmajestic/kraken-portfolio
+2. Run the Streamlit app using the following command:
+
+```
+git clone https://github.dev/mattmajestic/kraken-portfolio
+streamlit run app.py
+```
--- app.py
+++ app.py
@@ -7,11 +7,16 @@ import hmac
 import base64
 import time
 import pandas as pd
+import plotly.graph_objects as go
 
 # Read Kraken API key and secret stored in environment variables
 api_url = "https://api.kraken.com"
 api_key = os.environ['API_KEY_KRAKEN']
 api_sec = os.environ['API_SEC_KRAKEN']
+
+# Read the contents of the README.md file
+with open('README.md', 'r') as file:
+    readme_text = file.read()
 
 def get_kraken_signature(urlpath, data, secret):
     postdata = urllib.parse.urlencode(data)
@@ -44,9 +49,28 @@ def get_crypto_balances():
     return balances
 
 if __name__ == "__main__":
+    # Set page title and favicon
+    st.set_page_config(page_title="Kraken Crypto Balances", page_icon=":moneybag:")
+
+    st.markdown(readme_text, unsafe_allow_html=True)
+
     # Fetch cryptocurrency balances from Kraken
     balances = get_crypto_balances()
 
+    # Create a summary above the chart and datatable
+    total_balance = sum(float(balance) for balance in balances.values())
+    st.write(f"Total Balance: ${total_balance:.2f}")
+
     # Create a DataFrame to display the cryptocurrency balances
     df = pd.DataFrame({'Cryptocurrency': list(balances.keys()), 'Balance': list(balances.values())})
-    st.dataframe(df)
+
+    # Create two columns for chart and datatable side by side
+    col1, col2 = st.columns(2)
+
+    # Plot the pie chart in the first column
+    fig = go.Figure(data=go.Pie(labels=df['Cryptocurrency'], values=df['Balance']))
+    fig.update_layout(title='Crypto Account Balances', showlegend=False)
+    col1.plotly_chart(fig)
+
+    # Display the data table in the second column
+    col2.dataframe(df)
