6290555891b493e183ff969d24500c3715ef60bc
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-09-03 12:55:09 +0000

    view ['data']

--- app.py
+++ app.py
@@ -74,14 +74,13 @@ if __name__ == "__main__":
     coin_types_df = pd.read_csv('kraken_lookup.csv')
     # Replace with supabase fetch
     coin_types_db = supabase_client.table('kraken').select("*").execute()
-    coin_types_pd = pd.json_normalize(coin_types_db)
-    st.write(coin_types_pd)
+    st.write(coin_types_db['data'])
 
     # Merge coin types with balances data
     merged_data = pd.merge(pd.DataFrame(balances.items(), columns=['kraken_name', 'Balance']), coin_types_df, on='kraken_name', how='left')
     # Store to supabase via insert
-    merged_data_js = pd.json_normalize(merged_data)
-    merged_data_db, merged_data_count = supabase_client.table('kraken_balances').insert(merged_data_js).execute()
+    # merged_data_js = pd.json_normalize(merged_data)
+    # merged_data_db, merged_data_count = supabase_client.table('kraken_balances').insert(merged_data_js).execute()
 
     st.write("")
     st.write("## Portfolio Breakdown by Coin Type")
